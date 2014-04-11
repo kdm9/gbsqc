efa2c07074967bc2bd43e771f491e890a820f7a8
Kevin Murray <spam@kdmurray.id.au>
2014-04-11 14:43:36 +1000

    [+ split.sh] split out fq files by barcodes

---
+++ split.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd reads
+for i in {1..4} 6; do mkdir -p $i; done
+
+parallel --gnu \
+	pushd {}\; \
+	echo {} \; \
+	time sabre pe -f ../orig/plate{}_R1.fq.gz -r ../orig/plate{}_R2.fq.gz -b ../../bd.sabre -u {}_nobcd_R1.fq -w {}_nobcd_R2.fq -m 1\; \
+	popd \
+	::: {1..4} 6
