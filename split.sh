ed0b9206d09a9c0bb0251e12c028c197300acc37
Kevin Murray <spam@kdmurray.id.au>
2014-04-14 13:34:06 +1000

    [split.sh] use proper tool dir

--- split.sh
+++ split.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+tooldir=$(dirname $(readlink -f $0))/tools
 cd reads
 for i in {1..4} 6; do mkdir -p $i; done
 
 parallel --gnu \
 	pushd {}\; \
 	echo {} \; \
-	time ../tools/sabre/sabre pe -f ../orig/plate{}_R1.fq.gz -r ../orig/plate{}_R2.fq.gz -b ../../bd.sabre -u {}_nobcd_R1.fq -w {}_nobcd_R2.fq -m 1\; \
+	time $tooldir/sabre/sabre pe -f ../orig/plate{}_R1.fq.gz -r ../orig/plate{}_R2.fq.gz -b ../../bd.sabre -u {}_nobcd_R1.fq -w {}_nobcd_R2.fq -m 1\; \
 	popd \
 	::: {1..4} 6
