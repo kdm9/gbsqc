256f7a048b938a92328d90d2f2a439190ad8ea03
Kevin Murray <spam@kdmurray.id.au>
2014-04-14 13:25:07 +1000

    [merge+qualfilter] fix up bugs and disable debug

--- merge+qualfilter.sh
+++ merge+qualfilter.sh
@@ -14,12 +14,12 @@ do
 		# Sliding window trim. -q 26 = min window PHRED of 26, -c/m for interleaved files
 		# Save a copy to disk as well as big catted file
 
-	ls  --color=never [ACGT]*_1.fq |head -n 2 | parallel \
+	ls  --color=never [ACGT]*_1.fq | parallel -j 12 \
 		bcd\=\$\(basename {}  _1.fq\) \; \
 		${tooldir}/seqtkwithpatches/seqtk interleave \$\{bcd\}_[12].fq \| \
 	       		${tooldir}/seqtkwithpatches/seqtk seq -b \$bcd -B \$bcd - \| \
 			${tooldir}/scythe/scythe -p 0.05 -a ~/ws/refseqs/illumina_adapters.fa -M 0 - \| \
-			${tooldir}/sickle/sickle pe -c - -t sanger -m - -q 26 -n \| \
-			tee \$\{bcd\}_qcd.ilfq |gzip \>\> plate$i.ilfq.gz
+			${tooldir}/sickle/sickle pe -c /dev/stdin -t sanger -s \$\{bcd\}_singles.fq -m /dev/stdout -q 26 -n \| \
+			tee \$\{bcd\}_qcd.ilfq \| gzip \>\> plate$i.ilfq.gz
 	popd
 done
