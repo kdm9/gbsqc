95b0ab28756d70b0f2c152cd44597c08fab8b8f7
Kevin Murray <spam@kdmurray.id.au>
2014-04-15 17:35:18 +1000

    [merge+qualfilter] make logging better

--- merge+qualfilter.sh
+++ merge+qualfilter.sh
@@ -16,15 +16,17 @@ do
 		# Save a copy to disk as well as big catted file
 	# calls to Seqqs record the quality info to files for each stage.
 
-	ls  --color=never [ACGT]*_1.fq | parallel -j 14 --results log \
+	mkdir -p qc
+	mkdir -p log
+	ls  --color=never [ACGT]*_1.fq | parallel -j 14 --results log --joblog log/${i}_jlog_`date +%F_%T` \
 		bcd\=\$\(basename {}  _1.fq\) \; \
 		${tooldir}/seqtkwithpatches/seqtk interleave \$\{bcd\}_[12].fq \| \
-			${tooldir}/seqqs/seqqs -i -e -p \$\{bcd\}_initialqc - \| \
+			${tooldir}/seqqs/seqqs -i -e -p qc/\$\{bcd\}_initial - \| \
 	       		${tooldir}/seqtkwithpatches/seqtk seq -b \$bcd -B \$bcd - \| \
 			${tooldir}/scythe/scythe -p 0.05 -a ~/ws/refseqs/illumina_adapters.fa -M 0 - \| \
-			${tooldir}/seqqs/seqqs -i -e -p \$\{bcd\}_afterscythe - \| \
-			${tooldir}/sickle/sickle pe -c /dev/stdin -t sanger -s \$\{bcd\}_singles.fq -m /dev/stdout -q 26 -n \| \
-			${tooldir}/seqqs/seqqs -i -e -p \$\{bcd\}_aftersickle - \| \
+			${tooldir}/seqqs/seqqs -i -e -p qc/\$\{bcd\}_scythe - \| \
+			${tooldir}/sickle/sickle pe -c /dev/stdin -t sanger -s \>\(gzip \>\$\{bcd\}_s.fq.gz\) -m /dev/stdout -q 26 -l 64 -n \| \
+			${tooldir}/seqqs/seqqs -i -e -p qc/\$\{bcd\}_sickle - \| \
 			gzip \>\> plate$i.ilfq.gz
 	popd
 done
